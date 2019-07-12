d80d390b0aa411ee575da28dfc8690a773585a59
Helmi Ruza <helmiruza@gmail.com>
2019-07-12 18:00:34 +0800

    Add GetBill & CreateBill

---
+++ bill.go
@@ -0,0 +1,68 @@
+package billplz
+
+import (
+  "fmt"
+	"net/http"
+  "io/ioutil"
+  "log"
+  "bytes"
+  "encoding/json"
+  models "github.com/helmiruza/billplz-go/models"
+)
+
+const (
+  productionUrl = "https://billplz.com"
+	stagingUrl = "https://billplz-staging.herokuapp.com"
+)
+
+var ENVIRONTMENT = ""
+var APIKEY = ""
+var URL = ""
+
+func Init(e string, f string) {
+	ENVIRONTMENT = e
+  APIKEY = f
+
+  if ENVIRONTMENT == "production" {
+    URL = productionUrl
+  }
+
+  if ENVIRONTMENT == "staging" {
+    URL = stagingUrl
+  }
+}
+
+func GetBill(billId string) (string) {
+  client := &http.Client{}
+
+  URL += fmt.Sprintf("/api/v4/bills/%s", billId)
+
+  req, _ := http.NewRequest("GET", URL, nil)
+  req.SetBasicAuth(APIKEY, "")
+
+  resp, _ := client.Do(req)
+  body, _ := ioutil.ReadAll(resp.Body)
+	s := string(body)
+  return s
+}
+
+func CreateBill(data models.Bill) (string) {
+  URL += "/api/v4/bills"
+  requestBody, _ := json.Marshal(data)
+
+  client := &http.Client{}
+
+  req, _ := http.NewRequest("POST", URL, bytes.NewBuffer(requestBody))
+  req.SetBasicAuth(APIKEY, "")
+  req.Header.Set("Content-type", "application/json")
+
+  resp, _ := client.Do(req)
+
+  body, err := ioutil.ReadAll(resp.Body)
+  if err != nil {
+    log.Fatalln(err)
+  }
+
+  s := string(body)
+  return s
+}
---
+++ models/bill.go
@@ -0,0 +1,18 @@
+package models
+
+type Bill struct {
+  CollectionId string `json:"collection_id"`
+  Email string `json:"email"`
+  Mobile string `json:"mobile"`
+  Name string `json:"name"`
+  Amount int `json:"amount"`
+  CallbackUrl string `json:"callback_url"`
+  Description string `json:"description"`
+  DueAt string `json:"due_at"`
+  RedirectUrl string `json:"redirect_url"`
+  Deliver bool `json:"deliver"`
+  Reference1Lable string `json:"reference_1_label"`
+  Reference1 string `json:"reference_1"`
+  Reference2Lable string `json:"reference_2_label"`
+  Reference2 string `json:"reference_2"`
+}
